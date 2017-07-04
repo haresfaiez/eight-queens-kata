0120a732b7a27c414663f0846d8374cf2a8bedd3
Hares Faiez <faiez.hrs@gmail.com>
2017-07-04 22:44:54 +0100

    Generalize the scope of the free diagonal search to the initial diagonal

--- src/main/java/solution/Queen.java
+++ src/main/java/solution/Queen.java
@@ -2,42 +2,60 @@ package solution;
 
 public class Queen {
 
-  Integer row;
-  Integer column;
+    Integer row;
+    Integer column;
 
-  Queen(Integer aRow, Integer aColumn) {
-    this.row    = aRow;
-    this.column = aColumn;
-  }
-
-  public Object freeDiagonal(Queen existing) {
-    Queen result = this;
-    if (result.isInDiagonalConflitWith(existing)) {
-	result = result.verticalIncrement(0);
+    Queen(Integer aRow, Integer aColumn) {
+	this.row    = aRow;
+	this.column = aColumn;
     }
-    return result.row;
-  }
 
-  private boolean isInDiagonalConflitWith(Queen aTarget) {
-    return (2 == this.row) && (2 == this.column)
-        && (1 == aTarget.row) && (1 == aTarget.column);
-  }
+    public Object freeDiagonal(Queen existing) {
+	Queen result = this;
+	// After next: Remove the conditional
+	if (result.isInDiagonalConflitWith(existing)) {
+	    result = result.verticalIncrement(0);
+	}
+	return result.row;
+    }
 
-  public Queen verticalIncrement(Integer resultColumn) {
-    return new Queen((this.row + 1) % 8, resultColumn);
-  }
+    private boolean isInDiagonalConflitWith(Queen aTarget) {
+	// Next: Clean this
+	Boolean result = false;
+	for (int i = 1; i < 9; i++) {
+	    Boolean isRowi = (1 + i == this.row) && (1 + i == this.column)
+		          && (1 == aTarget.row) && (1 == aTarget.column); 
+	
+	    result = result || (isRowi);
+	}
+	return result;
+    }
 
-  public Queen next() {
-    return new Queen(this.row + 2, this.column + 1);
-  }
+    public Queen verticalIncrement(Integer resultColumn) {
+	Integer resultRow;
 
-  @Override
-  public boolean equals(Object anOther) {
-    return ((Queen) anOther).column.equals(this.column)
-        && ((Queen) anOther).row.equals(this.row);
-  }
+	if (8 == this.row) resultRow = 1;
+	else               resultRow = this.row + 1;
 
-  public static Queen at(Integer aRow, Integer aColumn) {
-    return new Queen(aRow, aColumn);
-  }
+	return new Queen(resultRow, resultColumn);
+    }
+
+    public Queen next() {
+	return new Queen(this.row + 2, this.column + 1);
+    }
+
+    @Override
+    public boolean equals(Object anOther) {
+	return ((Queen) anOther).column.equals(this.column)
+	    && ((Queen) anOther).row.equals(this.row);
+    }
+
+    @Override
+    public String toString() {
+	return String.format("Queen(%s, %s)", this.row, this.column);
+    }
+
+    public static Queen at(Integer aRow, Integer aColumn) {
+	return new Queen(aRow, aColumn);
+    }
 }
--- src/test/java/QueenIncrementTest.java
+++ src/test/java/QueenIncrementTest.java
@@ -6,6 +6,12 @@ import solution.Queen;
 public class QueenIncrementTest {
 
   @Test
+  public void freeDiagonalDoesAlignWithTopLeftQueen() {
+      Queen seven_x_seven = Queen.at(7, 7);
+      assertEquals(8, seven_x_seven.freeDiagonal(Queen.at(1, 1)));
+  }
+    
+  @Test
   public void incrementOfLastRowIsTheFirstRow() {
     assertEquals(Queen.at(1, 1), Queen.at(8, 1).verticalIncrement(1));
   }
